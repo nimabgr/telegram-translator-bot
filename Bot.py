acca4fa8a248f16a97f62dc6f0f1658d6725146a
nimabgr <nimabagherzadehw@gmail.com>
2025-10-14 23:03:50 +0330

    Add telegram bot source code and setup files

---
+++ Bot.py
@@ -0,0 +1,152 @@
+import os
+import telebot
+from googletrans import Translator
+
+translator = Translator()
+LANGUAGES = {
+    'af': 'afrikaans',
+    'sq': 'albanian',
+    'am': 'amharic',
+    'ar': 'arabic',
+    'hy': 'armenian',
+    'az': 'azerbaijani',
+    'eu': 'basque',
+    'be': 'belarusian',
+    'bn': 'bengali',
+    'bs': 'bosnian',
+    'bg': 'bulgarian',
+    'ca': 'catalan',
+    'ceb': 'cebuano',
+    'ny': 'chichewa',
+    'zh-cn': 'chinese (simplified)',
+    'zh-tw': 'chinese (traditional)',
+    'co': 'corsican',
+    'hr': 'croatian',
+    'cs': 'czech',
+    'da': 'danish',
+    'nl': 'dutch',
+    'en': 'english',
+    'eo': 'esperanto',
+    'et': 'estonian',
+    'tl': 'filipino',
+    'fi': 'finnish',
+    'fr': 'french',
+    'fy': 'frisian',
+    'gl': 'galician',
+    'ka': 'georgian',
+    'de': 'german',
+    'el': 'greek',
+    'gu': 'gujarati',
+    'ht': 'haitian creole',
+    'ha': 'hausa',
+    'haw': 'hawaiian',
+    'iw': 'hebrew',
+    'he': 'hebrew',
+    'hi': 'hindi',
+    'hmn': 'hmong',
+    'hu': 'hungarian',
+    'is': 'icelandic',
+    'ig': 'igbo',
+    'id': 'indonesian',
+    'ga': 'irish',
+    'it': 'italian',
+    'ja': 'japanese',
+    'jw': 'javanese',
+    'kn': 'kannada',
+    'kk': 'kazakh',
+    'km': 'khmer',
+    'ko': 'korean',
+    'ku': 'kurdish (kurmanji)',
+    'ky': 'kyrgyz',
+    'lo': 'lao',
+    'la': 'latin',
+    'lv': 'latvian',
+    'lt': 'lithuanian',
+    'lb': 'luxembourgish',
+    'mk': 'macedonian',
+    'mg': 'malagasy',
+    'ms': 'malay',
+    'ml': 'malayalam',
+    'mt': 'maltese',
+    'mi': 'maori',
+    'mr': 'marathi',
+    'mn': 'mongolian',
+    'my': 'myanmar (burmese)',
+    'ne': 'nepali',
+    'no': 'norwegian',
+    'or': 'odia',
+    'ps': 'pashto',
+    'fa': 'persian',
+    'pl': 'polish',
+    'pt': 'portuguese',
+    'pa': 'punjabi',
+    'ro': 'romanian',
+    'ru': 'russian',
+    'sm': 'samoan',
+    'gd': 'scots gaelic',
+    'sr': 'serbian',
+    'st': 'sesotho',
+    'sn': 'shona',
+    'sd': 'sindhi',
+    'si': 'sinhala',
+    'sk': 'slovak',
+    'sl': 'slovenian',
+    'so': 'somali',
+    'es': 'spanish',
+    'su': 'sundanese',
+    'sw': 'swahili',
+    'sv': 'swedish',
+    'tg': 'tajik',
+    'ta': 'tamil',
+    'te': 'telugu',
+    'th': 'thai',
+    'tr': 'turkish',
+    'uk': 'ukrainian',
+    'ur': 'urdu',
+    'ug': 'uyghur',
+    'uz': 'uzbek',
+    'vi': 'vietnamese',
+    'cy': 'welsh',
+    'xh': 'xhosa',
+    'yi': 'yiddish',
+    'yo': 'yoruba',
+    'zu': 'zulu',}
+
+showLAN = str(LANGUAGES)
+Language = "fa"
+
+API_KEY = os.getenv('API_KEY')
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    print(message)
+    bot.reply_to(message, "hello , welcome to our bot , send a text and i translate it to you !!! :)")
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.reply_to(message, "/allLang showing all available dist Lang\n /dest set your dist Lan\n /T translate your text ")
+
+@bot.message_handler(commands=['allLang'])
+def show_destination_language(message):
+        bot.reply_to(message , showLAN)
+
+@bot.message_handler(commands=['dest'])
+def dest(message):
+    global Language
+    Language = message.text.split(" ")[1]
+    bot.reply_to(message , f"destination language set to {LANGUAGES.get(Language)}")
+    
+
+@bot.message_handler(commands=['show'])
+def show(message):
+     bot.reply_to(message , Language)
+
+
+@bot.message_handler(commands=['T'])
+def translate(message):
+    text_to_translate = message.text.split("T" , 1)[1]
+    print(Language)
+    result = translator.translate(text_to_translate , dest=Language).text
+    bot.reply_to(message , result)
+bot.polling()   
--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# telegram-translator-bot
-A simple Telegram bot that translates text using the googletrans library. Developed as a SAAS project for a Cloud Computing course
+# Telegram Translator Bot 
+
+A simple Telegram bot that translates text into different languages. This project was developed as a Software as a Service (SAAS) for a university Cloud Computing course.
+
+## Features
+
+* Translate any text to a specified destination language.
+* Set a default destination language for translations.
+* List all available languages for translation.
+* Easy to deploy and run.
+
+## Deployment (SAAS Model)
+
+This bot is a practical implementation of the **Software as a Service (SAAS)** model. Instead of requiring users to run the code on their own machines, the bot is deployed and runs continuously on a cloud platform, providing translation services on demand.
+
+* **Platform:** The application is hosted on **PythonAnywhere**, a cloud-based platform (PaaS) that allows for easy deployment and management of Python web applications.
+* **SAAS Concept:** Users interact with the service (the translator bot) via the Telegram interface without needing to worry about the underlying code, servers, or maintenance. The service is available 24/7, just like any commercial SAAS product. This approach was central to fulfilling the requirements of the Cloud Computing course.
+
+## How to Run Locally
+
+If you want to run your own instance of the bot, follow these steps:
+
+1.  **Clone the repository:**
+    ```bash
+    git clone [https://github.com/nimabgr/telegram-translator-bot.git](https://github.com/nimabgr/telegram-translator-bot.git)
+    cd telegram-translator-bot
+    ```
+
+2.  **Create a virtual environment:**
+    ```bash
+    python -m venv venv
+    source venv/bin/activate  # On Windows use `venv\Scripts\activate`
+    ```
+
+3.  **Install the dependencies:**
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+4.  **Set up your environment variables:**
+    You need an API key from BotFather on Telegram. Create a file named `.env` in the project root and add your API key:
+    ```
+    API_KEY="YOUR_TELEGRAM_BOT_API_KEY"
+    ```
+    *Note: The `.env` file is included in `.gitignore` and will not be uploaded to GitHub.*
+
+5.  **Run the bot:**
+    ```bash
+    python Bot.py
+    ```
+
+## Technologies Used
+
+* **Language:** Python
+* **Libraries:**
+    * [pyTelegramBotAPI](https://github.com/eternnoir/pyTelegramBotAPI)
+    * [googletrans](https://pypi.org/project/googletrans/)
+* **Deployment:** PythonAnywhere (Cloud Platform)
